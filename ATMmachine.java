0c95ee535659665ca39750b2159333927bb2b813
Kajal Gond <kajalgond7266@gmail.com>
2024-10-17 22:25:52 -0800

    TASK3

---
+++ ATMmachine.java
@@ -0,0 +1,77 @@
+package ATMInterface;
+import java.util.Scanner;
+
+class ATM{
+    Float Balance;
+    int PIN=6307;
+    public void checkpin(){
+        System.out.println("Enter your PIN: ");
+        Scanner sc = new Scanner(System.in);
+        int enterPin = sc.nextInt();
+        if(enterPin==PIN){
+            menu();
+        }
+        else{
+            System.out.println("Enter a Valid PIN");
+        }
+    }
+    public void menu(){
+        System.out.println("Enter Your Choice: ");
+        System.out.println("1. Check A/C Balance ");
+        System.out.println("2. Withdraw Money ");
+        System.out.println("3. Deposit Money ");
+        System.out.println("4. Exit ");
+
+        Scanner sc = new Scanner(System.in);
+        int opt = sc.nextInt();
+
+        if(opt==1){
+            checkBalance();
+        }
+        else if (opt==2) {
+            withdrawMoney();
+        }
+        else if (opt==3) {
+            depositMoney();
+        }
+        else if (opt==4) {
+           return;
+        }
+        else{
+            System.out.println("Enter a valid choice");
+        }
+    }
+    public void checkBalance(){
+        System.out.println("Balance: " + Balance);
+        menu();
+    }
+    public void withdrawMoney(){
+        System.out.println("Enter Amount to Withdraw: ");
+        Scanner sc= new Scanner(System.in);
+        float amount = sc.nextFloat();
+        if(amount>Balance){
+            System.out.println("Insufficient Balance");
+        }
+        else{
+            Balance= Balance-amount;
+            System.out.println("Money Withdrawl Successful");
+
+        }
+        menu();
+    }
+    public void depositMoney(){
+        System.out.println("Enter the Amount: ");
+        Scanner sc= new Scanner(System.in);
+        float amount= sc.nextInt();
+        Balance = Balance + amount;
+        System.out.println("Money Deposited Successfully");
+    }
+
+}
+public class ATMmachine {
+    public static void main(String[] args) {
+        ATM obj= new ATM();
+        obj.checkpin();
+
+    }
+}
